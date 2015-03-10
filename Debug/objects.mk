ea611624cc777d702c0e5fcf65b1865296532b3c
Timofey <tsitnikov@muellercompany.com>
2015-03-10 17:24:47 -0400

    Deleted mk files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"/work/c1202-firmware/driverlib/Debug/driverlib.lib" -l"libmath.a"
-
